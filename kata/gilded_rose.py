cc5ac72298266d13736f6a2bb978a477d9841f07
MPilkou <mpilkou@gmail.com>
2018-11-26 20:14:47 +0100

    SM rules

--- kata/gilded_rose.py
+++ kata/gilded_rose.py
@@ -7,36 +7,60 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-                    # added
-                    if "conjured" in item.name.lower():
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+            self.update_item(item)
+    
+    def my_if_1(self,item):
+        if item.quality > 0:
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                item.quality = item.quality - 1
+            # added
+            if "conjured" in item.name.lower():
+                item.quality = item.quality - 1
+    
+    def my_else_1(self,item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.my_else_1_1(item)
+                
+
+    def my_else_1_1(self,item):
+        if item.sell_in < 11:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        if item.sell_in < 6:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+    
+    def my_diff_1(self,item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            self.my_diff_1_1(item)
+            
+    
+    def my_diff_1_1(self,item):
+        if item.name != "Aged Brie":
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                self.my_diff_1_1_1(item)
+                
+            else:
+                item.quality = item.quality - item.quality
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+
+    def my_diff_1_1_1(self,item):
+        if item.quality > 0:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = item.quality - 1
+
+    def update_item(self,item):
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            self.my_if_1(item)
+        else:
+            self.my_else_1(item)
+        self.my_diff_1(item)
 
 
 class Item:
